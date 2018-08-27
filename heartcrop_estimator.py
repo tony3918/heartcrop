0b829a1b00f9da97329b8503be11216265a79b7c
user <tony3918@hotmail.com>
2018-08-27 22:24:53 +0900

    fixed bug  : test results for sagital and coronal were calculated using classifier_axial

--- heartcrop_estimator.py
+++ heartcrop_estimator.py
@@ -2,11 +2,13 @@ from AlexNet import AlexNet
 from utils import gather_2_5D_data
 import tensorflow as tf
 import numpy as np
-import os, time
+import os, argparse
+import matplotlib.pyplot as plt
 
 tf.logging.set_verbosity(tf.logging.INFO)
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["CUDA_VISIBLE_DEVICES"]="0,2"
+DATA_DIR = '/home/user/tony/DATA/np/ct'
 
 def heartcrop_model_fnn(features, labels, mode):
     input = tf.reshape(features["x"], [-1,256,256,1])
@@ -39,39 +41,19 @@ def heartcrop_model_fnn(features, labels, mode):
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
-def train():
-    DATA_DIR = '/home/user/tony/DATA/np/ct'
-    images_axial = []
-    images_sagital = []
-    images_coronal = []
-    for patient_id in sorted(os.listdir(DATA_DIR))[:50]:
-        ct = np.load(os.path.join(DATA_DIR, patient_id))
-        images_axial.append(ct)
-        images_sagital.append(np.transpose(ct, [2, 0, 1]))
-        images_coronal.append(np.transpose(ct, [1, 0, 2]))
-    images_axial = np.concatenate([ct for ct in images_axial]).astype(np.float32)
-    images_sagital = np.concatenate([ct for ct in images_sagital]).astype(np.float32)
-    images_coronal = np.concatenate([ct for ct in images_coronal]).astype(np.float32)
+def train(num_patients):
+    if not num_patients:
+        num_patients = 50
+    patient_list = sorted(os.listdir(DATA_DIR))[:num_patients]
+    (train_images_axial, train_labels_axial,
+     train_images_sagital, train_labels_sagital,
+     train_images_coronal, train_labels_coronal) = gather_2_5D_data(patient_list, training=False)
 
-    labels_axial = []
-    labels_sagital = []
-    labels_coronal = []
-    with open('/home/user/tony/CODE/heartcrop_estimator/true_roi.txt', 'r') as f:
-        for line in f.readlines()[:50]:
-            id, x1, y1, z1, x2, y2, z2 = [int(i) for i in line.strip().split('\t')]
-            label = np.zeros([256, 256, 256], dtype=np.int8)
-            label[z1:z2, y1:y2, x1:x2] = 1
-            labels_axial.append(label)
-            labels_sagital.append(np.transpose(label, [2, 0, 1]))
-            labels_coronal.append(np.transpose(label, [1, 0, 2]))
-    labels_axial = np.any(np.concatenate([label for label in labels_axial]), axis=(1, 2)).astype(np.int32)
-    labels_sagital = np.any(np.concatenate([label for label in labels_sagital]), axis=(1, 2)).astype(np.int32)
-    labels_coronal = np.any(np.concatenate([label for label in labels_coronal]), axis=(1, 2)).astype(np.int32)
+    print(f'Train image shape: {train_images_coronal.shape}')
+    print(f'Train image type: {train_images_coronal.dtype}')
+    print(f'Train label shape: {train_labels_coronal.shape}')
+    print(f'Train label type: {train_labels_coronal.dtype}')
 
-    print(f'Train image shape: {images_axial.shape}')
-    print(f'Train image type: {images_axial.dtype}')
-    print(f'Train label shape: {labels_axial.shape}')
-    print(f'Train label type: {labels_axial.dtype}')
 
     config = tf.estimator.RunConfig(log_step_count_steps=100)
     classifier_axial = tf.estimator.Estimator(model_fn=heartcrop_model_fnn,
@@ -88,50 +70,29 @@ def train():
         return tf.estimator.inputs.numpy_input_fn(x={"x":images},
                                                   y=labels,
                                                   batch_size=128,
-                                                  num_epochs=30,
+                                                  num_epochs=10,
                                                   shuffle=True)
     print("TRAINING AXIAL")
-    classifier_axial.train(input_fn=train_input_fn(images_axial, labels_axial))
+    classifier_axial.train(input_fn=train_input_fn(train_images_axial, train_labels_axial))
     print("TRAINING SAGITAL")
-    classifier_sagital.train(input_fn=train_input_fn(images_sagital, labels_sagital))
+    classifier_sagital.train(input_fn=train_input_fn(train_images_sagital, train_labels_sagital))
     print("TRAINING CORONAL")
-    classifier_coronal.train(input_fn=train_input_fn(images_coronal, labels_coronal))
+    classifier_coronal.train(input_fn=train_input_fn(train_images_coronal, train_labels_coronal))
     print("FINISHED TRAINING")
 
 
-def test():
-    DATA_DIR = '/home/user/tony/DATA/np/ct'
-    images_axial = []
-    images_sagital = []
-    images_coronal = []
-    for patient_id in sorted(os.listdir(DATA_DIR))[50:]:
-        ct = np.load(os.path.join(DATA_DIR, patient_id))
-        images_axial.append(ct)
-        images_sagital.append(np.transpose(ct, [2, 0, 1]))
-        images_coronal.append(np.transpose(ct, [1, 0, 2]))
-    images_axial = np.concatenate([ct for ct in images_axial]).astype(np.float32)
-    images_sagital = np.concatenate([ct for ct in images_sagital]).astype(np.float32)
-    images_coronal = np.concatenate([ct for ct in images_coronal]).astype(np.float32)
+def test(num_patients):
+    if not num_patients:
+        num_patients = 12
+    patient_list = sorted(os.listdir(DATA_DIR))[-num_patients:]
+    (test_images_axial, test_labels_axial,
+     test_images_sagital, test_labels_sagital,
+     test_images_coronal, test_labels_coronal) = gather_2_5D_data(patient_list)
 
-    labels_axial = []
-    labels_sagital = []
-    labels_coronal = []
-    with open('/home/user/tony/CODE/heartcrop_estimator/true_roi.txt', 'r') as f:
-        for line in f.readlines()[50:]:
-            id, x1, y1, z1, x2, y2, z2 = [int(i) for i in line.strip().split('\t')]
-            label = np.zeros([256, 256, 256], dtype=np.int8)
-            label[z1:z2, y1:y2, x1:x2] = 1
-            labels_axial.append(label)
-            labels_sagital.append(np.transpose(label, [2, 0, 1]))
-            labels_coronal.append(np.transpose(label, [1, 0, 2]))
-    labels_axial = np.any(np.concatenate([label for label in labels_axial]), axis=(1, 2)).astype(np.int32)
-    labels_sagital = np.any(np.concatenate([label for label in labels_sagital]), axis=(1, 2)).astype(np.int32)
-    labels_coronal = np.any(np.concatenate([label for label in labels_coronal]), axis=(1, 2)).astype(np.int32)
-
-    print(f'Test image shape: {images_axial.shape}')
-    print(f'Test image type: {images_axial.dtype}')
-    print(f'Test label shape: {labels_axial.shape}')
-    print(f'Test label type: {labels_axial.dtype}')
+    print(f'Test image shape: {test_images_coronal.shape}')
+    print(f'Test image type: {test_images_coronal.dtype}')
+    print(f'Test label shape: {test_labels_coronal.shape}')
+    print(f'Test label type: {test_labels_coronal.dtype}')
 
     config = tf.estimator.RunConfig(log_step_count_steps=100)
     classifier_axial = tf.estimator.Estimator(model_fn=heartcrop_model_fnn,
@@ -150,18 +111,26 @@ def test():
                                                   num_epochs=1,
                                                   shuffle=False)
 
-    results_axial = classifier_axial.evaluate(input_fn=eval_input_fn(images_axial, labels_axial))
-    results_sagital = classifier_axial.evaluate(input_fn=eval_input_fn(images_sagital, labels_sagital))
-    results_coronal = classifier_axial.evaluate(input_fn=eval_input_fn(images_coronal, labels_coronal))
+    results_axial = classifier_axial.evaluate(input_fn=eval_input_fn(test_images_axial, test_labels_axial))
+    results_sagital = classifier_sagital.evaluate(input_fn=eval_input_fn(test_images_sagital, test_labels_sagital))
+    results_coronal = classifier_coronal.evaluate(input_fn=eval_input_fn(test_images_coronal, test_labels_coronal))
     print(f'Axial: {results_axial}')
     print(f'Sagital: {results_sagital}')
     print(f'Coronal: {results_coronal}')
 
 
-
 def main(unused_argv):
-    train()
-    test()
+    parser = argparse.ArgumentParser(description="Train or Test 2.5D CNN HeartCrop")
+    parser.add_argument("-tr", "--train", type=int, default=0, help="train model with n patient's data")
+    parser.add_argument("-te", "--test", type=int, default=0, help="test model with n patient's data")
+    args = parser.parse_args()
+    print("Started heartcrop_estimator.py")
+    if args.train:
+        print(f'Will train model on {args.train} patients')
+        train(args.train)
+    if args.test:
+        print(f'Will test model on {args.test} patients')
+        test(args.train)
 
 
 if __name__ == '__main__':
